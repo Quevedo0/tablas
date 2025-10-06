9e6e203e72563c0fcf4aeb2b0059740f855f4aba
Fabian Leonardo Loaiza Ramirez <fabianloaramirez1@gmail.com>
2025-10-06 15:18:32 -0700

    Tablas - Loaiza Ramirez Fabian Leonardo

---
+++ Tablas.rs
@@ -0,0 +1,20 @@
+//Tablas de multiplicar
+//Loaiza Ramirez Fabian Leonardo
+
+fn main() {
+
+    for i in 1..11 {
+        
+        println!("Tabla de {i}");
+        
+        for j in 1..11{
+            
+            println!("{} x {} = {}", i, j, i*j);    
+            
+        }
+       
+        println!("\n");
+        
+    }
+    
+}
