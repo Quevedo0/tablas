9f622a60bd8e19f7de20fd6ac7f4bcd928cffbfa
jdsicairos24 <jd.sicairos24@info.uas.edu.mx>
2025-10-06 11:03:34 -0700

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,19 @@
+//JOSE DAVID SICAIROS FONSECA
+ fn main(){
+
+for i in 1..=10 {
+    print!("la tabla del {}\n",i);
+
+        for j in 1..=10 {
+            print!("{} x {} = {}\n",i,j,i*j); 
+                   
+        }
+        print!("\n");
+    
+}
+
+
+
+
+
+ }
