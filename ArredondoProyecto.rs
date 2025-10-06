b1675cb8ecf4d3218c5008a9f8d923afce67a3c0
Arredondo Tapia <ft69120@gmail.com>
2025-10-06 11:04:21 -0700

    Add files via upload

---
+++ ArredondoProyecto.rs
@@ -0,0 +1,24 @@
+//Arredondo Tapia
+fn main() {
+
+    for i  in 1..=10 {
+        println!("Tabla del {}", i);
+
+
+        for j in 1..=10 {
+            println!("{} x {} = {}", i, j, i * j);
+        }
+        print!("\n");
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
