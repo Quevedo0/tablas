78b4e96be1250deb35d5e4744b28bb7ef3f14b5d
Diegotrevino <ds.trevino24@info.uas.edu.mx>
2025-10-06 11:01:27 -0700

    Diego Santos Treviño Camacho

---
+++ Diego_trevino_tablas.rs
@@ -0,0 +1,12 @@
+fn main() {
+//Diego Santos Treviño Camacho    
+
+ for i in 1..11{
+    println!("Tabla del {}: \n", i);
+    for j in 1..11{
+        println!("{} x {} = {}", i, j, i*j);
+    }
+        println!("\n");
+ }
+
+}
