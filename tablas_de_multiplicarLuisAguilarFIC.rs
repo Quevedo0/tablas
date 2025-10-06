dc7415dd9e986e9222d7aab9cb6d8e024ac88678
Lissandro <luismexico2006@gmail.com>
2025-10-06 14:12:44 -0700

    Add files via upload

---
+++ tablas_de_multiplicarLuisAguilarFIC.rs
@@ -0,0 +1,14 @@
+//use io::std;
+fn main () {
+
+    // Loops
+   for i in 1..=10 {
+      println!("Tablas de {}",i);
+      for j in 1..=10 {
+        println!("{} * {} = {}", i,j,i*j);
+        }
+
+        println!("\n");
+   }
+
+}
